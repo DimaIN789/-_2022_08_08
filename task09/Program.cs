9aa158a69e7db5c242c5307a59709cc2f728c136
Dmitrii Ivanov <diman_17az@mail.ru>
2022-08-08 22:50:59 +0300

    Семинар 2 от 8т августа + домашка

---
+++ task09/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 9. Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
+
+// методы возвратные и невозвратные
+
+int number = new Random().Next(10, 100);
+Console.WriteLine(number);
+int digit2 = number % 10;
+int digit1 = number / 10;
+if (digit1 > digit2)
+{
+    Console.WriteLine($"{digit1} - наибольшее число");
+}
+else
+{
+    Console.WriteLine($"{digit2}  - наибольшее число");
+}
---
+++ task16/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 16: Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8,9 -> нет
+
+
+Console.WriteLine("Введите первое целое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе целое число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Является ли одно число квадратом другого?");
+
+int num3 = num1 / num2;
+int num4 = num2 / num1;
+
+if (num3 == num2 || num4 == num1)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
+
